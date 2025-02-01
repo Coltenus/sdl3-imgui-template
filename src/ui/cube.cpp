1e303b376bf5b9df2459979e49610a81f1611a03
Maksym Yatskov <colten478@gmail.com>
2025-02-01 20:25:32 +0200

    added rotation speed for z axis

--- src/ui/cube.cpp
+++ src/ui/cube.cpp
@@ -43,7 +43,7 @@ void Cube::draw(float rotationAngle) {
 
     glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
     glm::mat4 projection = glm::perspective(glm::radians(45.0f), window_size.x / window_size.y, 0.1f, 100.0f); // Perspective projection
-    glm::mat4 model = glm::rotate(glm::mat4(1.0f), rotationAngle, glm::vec3(0.5f, 1.0f, 0.0f));
+    glm::mat4 model = glm::rotate(glm::mat4(1.0f), rotationAngle, glm::vec3(0.5f, 1.0f, 0.25f));
     shader->set_matrix4fv("model", model);
     shader->set_matrix4fv("view", view);
     shader->set_matrix4fv("projection", projection);
